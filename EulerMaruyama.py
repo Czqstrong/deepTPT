c095022cce042debe998d78dfe3fab792a3e1709
Czqstrong <58773374+Czqstrong@users.noreply.github.com>
2020-07-06 21:21:39 +0800

    Update EulerMaruyama.py

--- EulerMaruyama.py
+++ EulerMaruyama.py
@@ -4,49 +4,54 @@ import numpy as np
 from KDEpy import FFTKDE
 from scipy import stats
 from scipy.integrate import dblquad
+from bandwidth import kde2d
 
 # Initial data
-num = 10 ** 6
-x = stats.norm(loc=0, scale=1).rvs(num)
-y = stats.norm(loc=0, scale=1).rvs(num)
-b1 = np.zeros(num)
-b2 = np.zeros(num)
+num = 10 ** 7
+length = 10
+x = np.zeros([length, num])
+y = np.zeros([length, num])
+x[:, 0] = stats.norm(loc=0, scale=1).rvs(length)
+y[:, 0] = stats.norm(loc=0, scale=1).rvs(length)
+b1 = np.zeros(length)
+b2 = np.zeros(length)
 s1 = np.sqrt(2)
 s2 = np.sqrt(2)
 
-# Ending time
-T = 10000
-
 # Number of points in partition
-N = 200000
+N = 10 ** 7
 
 # Time increments
-dt = T / N
-norm = stats.norm(loc=0, scale=np.sqrt(0.5))
-x = norm.rvs(num)
-y = norm.rvs(num)
+dt = 0.001
+norm = stats.norm(loc=0, scale=np.sqrt(dt))
 
 # Iterate
-# for i in range(1, N):
-#     b1 = -2 * x * dt
-#     b2 = -2 * y * dt
-#     # b1 = -10 * x * (x ** 2 - 1) * dt
-#     # b2 = -10 * y * dt
-#     # b1 = 1.25 * (y - 0.5 * x) * dt
-#     # b2 = -200 * y * (y ** 2 - 1) * dt - 25 * (y - 0.5 * x) * dt
-#     x = x + b1 + s1 * norm.rvs(num)
-#     y = y + b2 + s2 * norm.rvs(num)
+for i in range(1, N):
+    b1 = -2 * x[:, i - 1] * dt
+    b2 = -2 * y[:, i - 1] * dt
+    # b1 = -10 * x[:, i - 1] * (x[:, i - 1] ** 2 - 1) * dt
+    # b2 = -10 * y[:, i - 1] * dt
+    x[:, i] = x[:, i - 1] + b1 + s1 * norm.rvs(length)
+    y[:, i] = y[:, i - 1] + b2 + s2 * norm.rvs(length)
+    print(i)
 
-plt.scatter(x, y)
+x_id = x[:, 10 ** 6:num + 1]  # 去掉前面1000个点
+y_id = y[:, 10 ** 6:num + 1]
+nd = length * (num - 10 ** 6)
+x_d = x_id.reshape(1, nd)
+y_d = y_id.reshape(1, nd)
+plt.scatter(x_d, y_d)
+nn = len(x_d)
 # kernel density estimation
-data = np.vstack((x, y)).T
-cov=np.cov(data.T)
+data = np.vstack((x_d, y_d)).T
+cov = np.cov(data.T)
 grid_points = 2 ** 7  # Grid points in each dimension
 N = 16  # Number of contours
 
-bandwidth = (num**(-1/6)) * cov[0,0]
-# Compute the kernel density estimate
-kde = FFTKDE(bw=0.09, norm=2)
+# bd = kde2d(data, nn)
+# # Compute the kernel density estimate
+# bandwidth = bd.compute()
+kde = FFTKDE(bw=0.05, norm=2)
 grid, points = kde.fit(data).evaluate(grid_points)
 
 # The grid is of shape (obs, dims), points are of shape (obs, 1)
@@ -97,3 +102,4 @@ ax3 = fig.add_subplot(1, 3, 3)
 plt.contourf(xx, yy, z3, N, cmap="RdBu_r")
 plt.colorbar()
 plt.show()
+
